79520250350c25592cb323723bb789bbe891711c
Justin <jstnwrds55@gmail.com>
2018-01-16 14:25:53 -0700

    added average functionality to scores

--- scores.cpp
+++ scores.cpp
@@ -4,20 +4,49 @@ using namespace std;
 
 int highestScore() {
 
-    int highestNumber = 0;
-    int currentNumber = 0;
-    
-    while (currentNumber != -1) {
+    cout << "Do you want to find the highest value (h) or average (a)? ";
+    char choice;
+    cin >> choice;
 
-        cout << "Enter a score (-1 to end): ";
-        cin >> currentNumber;
+    if (choice == 'h' || choice == 'H') {
 
-        if (currentNumber > highestNumber) {
-            highestNumber = currentNumber;
+        int highestNumber = 0;
+        int currentNumber = 0;
+
+        while (currentNumber != -1) {
+
+            cout << "Enter a score (-1 to end): ";
+            cin >> currentNumber;
+
+            if (currentNumber > highestNumber) {
+                highestNumber = currentNumber;
+            }
+
         }
 
+        cout << "The highest score is " << highestNumber << "." << endl;
+
+    } else if (choice == 'a' || choice == 'A') {
+
+        int average = 0;
+        int currentNumber = 0;
+        int numbers = 0;
+
+        while (currentNumber != -1) {
+
+            cout << "Enter a score (-1 to end): ";
+            cin >> currentNumber;
+            if (currentNumber == -1) {
+                break;
+            }
+            average += currentNumber;
+            numbers ++;
+
+        }
+
+        average /= numbers;
+        cout << "The average of those scores is " << average << ".";
     }
 
-    cout << "The highest score is " << highestNumber << "." << endl;
 }
 
