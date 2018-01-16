48f6c94cbbf68fcc2a97cd5687efb5b9131d2212
Justin <jstnwrds55@gmail.com>
2018-01-16 14:30:36 -0700

    Changed functions to minimize code

--- scores.cpp
+++ scores.cpp
@@ -11,16 +11,19 @@ int highestScore() {
     if (choice == 'h' || choice == 'H') {
 
         int highestNumber = 0;
-        int currentNumber = 0;
+        int currentNumber;
+
+        cout << "Enter a a score (-1 to end): ";
+        cin >> currentNumber;
 
         while (currentNumber != -1) {
-
-            cout << "Enter a score (-1 to end): ";
-            cin >> currentNumber;
 
             if (currentNumber > highestNumber) {
                 highestNumber = currentNumber;
             }
+
+            cout << "Enter a score (-1 to end): ";
+            cin >> currentNumber;
 
         }
 
@@ -29,22 +32,23 @@ int highestScore() {
     } else if (choice == 'a' || choice == 'A') {
 
         int average = 0;
-        int currentNumber = 0;
-        int numbers = 0;
+        int currentNumber;
+        int count = 0;
+
+        cout << "Enter a score (-1 to end): ";
+        cin >> currentNumber;
 
         while (currentNumber != -1) {
 
+            average += currentNumber;
+            count ++;
+
             cout << "Enter a score (-1 to end): ";
             cin >> currentNumber;
-            if (currentNumber == -1) {
-                break;
-            }
-            average += currentNumber;
-            numbers ++;
 
         }
 
-        average /= numbers;
+        average /= count;
         cout << "The average of those scores is " << average << ".";
     }
 
